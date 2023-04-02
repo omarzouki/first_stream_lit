e166e6e971e6037cd20df80036901a695e905c1e
omarzouki <128811513+omarzouki@users.noreply.github.com>
2023-04-02 12:15:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+
 streamlit.title("My parents new healthy diner");
 
 streamlit.header('🥣 Breakfast Menu')
@@ -12,29 +13,29 @@ streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
    
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
-
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice )
-#streamlit.text(fruityvice_response.json())
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')   
+   if not fruit_choice:
+      streamlite.error("Erreur merci de mettre un fruit")
+   else:
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice )      
+   # write your own comment -what does the next line do? 
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   # write your own comment - what does this do?
+   streamlit.dataframe(fruityvice_normalized)
+Except URLError as e:
+   streamlite.error()
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.write('The user entered ', fruit_choice)
+#streamlit.text(fruityvice_response.json())
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
